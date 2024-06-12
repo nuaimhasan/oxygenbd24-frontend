7dcd2e4a96f27ba808cc3dfbb08bee94367538d0
nuaimhasan <nuaim.emanager@gmail.com>
2024-06-12 16:55:50 +0600

    completed

--- src/pages/Admin/Dashboard/Dashboard.jsx
+++ src/pages/Admin/Dashboard/Dashboard.jsx
@@ -2,20 +2,30 @@ import { Link } from "react-router-dom";
 import { RiAdminFill } from "react-icons/ri";
 import { MdHomeRepairService } from "react-icons/md";
 import { LuImageMinus } from "react-icons/lu";
-// import Spinner from "../../../components/Spinner/Spinner";
+import { useGetAllProductsQuery } from "../../../Redux/product/productApi";
+import { useGetAdminsQuery } from "../../../Redux/user/userApi";
+import { useGetAllOrdersQuery } from "../../../Redux/order/orderApi";
+import { useGetAllRentOrdersQuery } from "../../../Redux/rentOrder/rentOrderApi";
+import { useGetAllRentsQuery } from "../../../Redux/rent/rentApi";
 
 export default function Dashboard() {
+  const { data: products } = useGetAllProductsQuery();
+  const { data: rent } = useGetAllRentsQuery();
+  const { data: admins } = useGetAdminsQuery();
+  const { data: orders } = useGetAllOrdersQuery();
+  const { data: rentOrders } = useGetAllRentOrdersQuery();
+
   return (
     <section>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
         <Link
-          to="/admin/administrator/all-administrator"
+          to="/admin/administrator"
           className="shadow rounded p-4 bg-base-100"
         >
           <div className="flex justify-between items-start">
             <div>
               <h2 className="text-lg">Total Admin</h2>
-              <p>1</p>
+              <p>{admins?.data?.length}</p>
             </div>
             <div>
               <RiAdminFill className="text-2xl text-secondary" />
@@ -23,11 +33,11 @@ export default function Dashboard() {
           </div>
         </Link>
 
-        <Link to="/admin/services" className="shadow rounded p-4 bg-base-100">
+        <Link to="/admin/products" className="shadow rounded p-4 bg-base-100">
           <div className="flex justify-between items-start">
             <div>
-              <h2 className="text-lg">Total Services</h2>
-              <p>0</p>
+              <h2 className="text-lg">Total Products</h2>
+              <p>{products?.data?.length}</p>
             </div>
             <div>
               <MdHomeRepairService className="text-2xl text-primary" />
@@ -35,11 +45,38 @@ export default function Dashboard() {
           </div>
         </Link>
 
-        <Link to="/admin/projects" className="shadow rounded p-4 bg-base-100">
+        <Link to="/admin/rental/all" className="shadow rounded p-4 bg-base-100">
           <div className="flex justify-between items-start">
             <div>
-              <h2 className="text-lg">Total Projects</h2>
-              <p>0</p>
+              <h2 className="text-lg">Total rent</h2>
+              <p>{rent?.data?.length}</p>
+            </div>
+            <div>
+              <LuImageMinus className="text-2xl text-primary" />
+            </div>
+          </div>
+        </Link>
+
+        <Link to="/admin/orders" className="shadow rounded p-4 bg-base-100">
+          <div className="flex justify-between items-start">
+            <div>
+              <h2 className="text-lg">Total Order</h2>
+              <p>{orders?.data?.length}</p>
+            </div>
+            <div>
+              <LuImageMinus className="text-2xl text-primary" />
+            </div>
+          </div>
+        </Link>
+
+        <Link
+          to="/admin/rent-orders"
+          className="shadow rounded p-4 bg-base-100"
+        >
+          <div className="flex justify-between items-start">
+            <div>
+              <h2 className="text-lg">Total Rent orders</h2>
+              <p>{rentOrders?.data?.length}</p>
             </div>
             <div>
               <LuImageMinus className="text-2xl text-primary" />
