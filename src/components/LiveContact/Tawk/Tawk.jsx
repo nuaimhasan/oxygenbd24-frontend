957a73a7703082c010181d5e0e9c5755459a7498
nuaimhasan <nuaim.emanager@gmail.com>
2024-07-13 18:01:44 +0600

    tawk code update

--- src/components/LiveContact/Tawk/Tawk.jsx
+++ src/components/LiveContact/Tawk/Tawk.jsx
@@ -8,7 +8,7 @@ export default function Tawk() {
       var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/667e48199d7f358570d43eb8/1i1ekj94e";
+      s1.src = "https://embed.tawk.to/6683ee0b9d7f358570d62461/1i1pljnic";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
